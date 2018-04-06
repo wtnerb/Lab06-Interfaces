0d99b82bcd0b91cb2527f395f3af4a4b9e2d3f97
wtnerb <wtnerb@gmail.com>
2018-04-06 09:31:26 -0700

    Added interface utilizing methods and printed things to the console.

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -10,6 +10,33 @@ namespace Lab06
     {
         static void Main(string[] args)
         {
+            Dragon smaug = new Dragon();
+            Lich xykon = new Lich("Xykon");
+            Console.WriteLine("the dragon worked his spell and incanted:");
+            ConsoleMagic(smaug);
+            Console.WriteLine();
+            Console.WriteLine("The lich cackled at your foolish naivette and explained:");
+            ConsoleRant(xykon);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// The entity of a class that implements ISpellcaster casts a spell and prints it to the console.
+        /// </summary>
+        /// <param name="mage">The instance of a spellcaster who is printing to the console.</param>
+        static void ConsoleMagic (ISpellcaster mage)
+        {
+            Console.WriteLine(mage.UseMagic());
+        }
+
+        /// <summary>
+        /// Many of my creatures like to talk at length. Those who have the ability implement ISpeak.
+        /// This takes their ranting and prints it to the console.
+        /// </summary>
+        /// <param name="loudly">a badly named parameter. This is an instance of a class that implements ISpeak and this specific instance will be speaking.</param>
+        static void ConsoleRant (ISpeak loudly)
+        {
+            Console.WriteLine(loudly.Speak());
         }
     }
 }
